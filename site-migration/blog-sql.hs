da8658ff3209394837bd3a0f0ca12e2f491b60bf
Michael Snoyman <michael@snoyman.com>
2011-09-15 08:12:35 +0300

    <foreign> blogs

--- site-migration/blog-sql.hs
+++ site-migration/blog-sql.hs
@@ -4,8 +4,8 @@ import qualified Data.Text as T
 import Network.HTTP.Enumerator (simpleHttp)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import System.Environment (getArgs)
-import Filesystem (listDirectory)
-import Filesystem.Path.CurrentOS ((</>), decodeString, filename, hasExtension, encodeString)
+import Filesystem (listDirectory, removeFile, removeDirectory, writeTextFile)
+import Filesystem.Path.CurrentOS ((</>), decodeString, filename, hasExtension, encodeString, directory, fromText, (</>), (<.>))
 import qualified Text.XML as X
 
 data BI = BI
@@ -18,17 +18,35 @@ data BI = BI
 main = do
     bis <- fmap read $ readFile "blog-infos.txt"
     sql' <- mapM sql bis
-    writeFile "blog-infos.sql" $ unlines $ "BEGIN TRANSACTION;" : sql' ++ ["ROLLBACK;"]
+    writeFile "blog-infos.sql" $ unlines $ "BEGIN TRANSACTION;" : "DELETE FROM \"Blog\";" : sql' ++ ["ROLLBACK;"]
 
 sql bi = do
     [file] <- fmap (filter $ flip hasExtension "ditamap") $ listDirectory $ "blogs" </> decodeString folder
-    X.Document _ (X.Element _ _ (X.NodeElement (X.Element _ _ [X.NodeContent title]):_)) _ <- X.readFile_ X.def $ encodeString file
+    X.Document _ (X.Element _ _ (X.NodeElement (X.Element _ _ [X.NodeContent title]):rest)) _ <- X.readFile_ X.def $ encodeString file
+    file' <- case rest of
+        [X.NodeElement (X.Element "topicref" [("id", _), ("href", href)] [])] -> do
+            let ditafile = directory file </> fromText href
+            X.Document _ root _ <- X.readFile_ X.def $ encodeString ditafile
+            case root of
+                X.Element _ _ [_, X.NodeElement (X.Element _ _ [X.NodeElement (X.Element "foreign" [("outputclass", oc)] [X.NodeContent content])])] -> do
+                    ext <-
+                        case oc of
+                            "html" -> return "html"
+                            "markdown" -> return "markdown"
+                            _ -> error $ "Unknown outputclass: " ++ show oc
+                    let newfile = directory file </> fromText (T.map noSlash title) <.> ext
+                    writeTextFile newfile content
+                    removeFile file
+                    removeFile ditafile
+                    removeDirectory $ directory ditafile
+                    return newfile
+                _ -> return file
+        _ -> return file
     let s = concat
             [ "INSERT INTO \"Blog\" (posted, contents, slug, year, month, author, title) VALUES('"
             , show $ biUTC bi
-            , "','home/1/blogs/"
-            , folder -- FIXME
-            , encodeString $ filename file
+            , "','home/1/"
+            , concatMap escape $ drop 2 $ encodeString file'
             , "','"
             , T.unpack $ biSlug bi
             , "',"
@@ -54,3 +72,5 @@ sql bi = do
         ]
     escape '\'' = "''"
     escape c = [c]
+    noSlash '/' = '-'
+    noSlash c = c
--- site-migration/run-all.sh
+++ site-migration/run-all.sh
@@ -9,10 +9,10 @@ curl $HOST/show/map/1/download > map-1.zip
 rm map-1.zip
 runghc add-chunking.hs book/yesod-web-framework-book/*.ditamap
 
-#rm -rf wiki page
-#runghc get-pages.hs $HOST
+rm -rf wiki page
+runghc get-pages.hs $HOST
 
-#runghc get-blogs.hs $HOST
+runghc get-blogs.hs $HOST
 rm -rf blogs
 mkdir blogs
 cd blogs
