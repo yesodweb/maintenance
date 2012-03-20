d3d8fb5e4475c7fb2ced4a401cc87e5e164fe97c
Michael Snoyman <michael@snoyman.com>
2012-03-20 17:48:05 +0200

    check-tarballs

--- release/check-tarballs.sh
+++ release/check-tarballs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
 rm -rf to-release && mkdir to-release
-runghc sdist-check.hs tarballs || (cabal install tar safe zlib-conduit unix-compat http-conduit filesystem-conduit && runghc sdist-check.hs tarballs)
+cabal-dev install
+./dist/build/sdist-check/sdist-check tarballs
